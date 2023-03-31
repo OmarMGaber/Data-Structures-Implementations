f0ef2cd28d250d6d186ccf9f4ee1f1a5c21b929c
Omar Muhammad <omarmgaber37@gmail.com>
2023-04-01 00:42:34 +0200

    Added StackArrayBased Methods

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,21 @@ import Stack.StackLinkedImplementation;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayBasedStackImplementation stack = new ArrayBasedStackImplementation(5);
+
+        stack.push(100);
+        stack.push(43);
+        stack.push(2);
+        stack.push(13);
+        ArrayBasedStackImplementation stack2 = new ArrayBasedStackImplementation(5);
+
+        stack2.push(12);
+        stack2.push(2);
+        stack2.push(43);
+        stack2.push(100);
+
+        System.out.println(stack2.isReversee(stack));
+
+        stack.printStack();
     }
 }
--- src/Stack/ArrayBasedStackImplementation.java
+++ src/Stack/ArrayBasedStackImplementation.java
@@ -5,7 +5,7 @@ public class ArrayBasedStackImplementation implements Stack {
     private int size;
     private int[] array;
 
-    ArrayBasedStackImplementation(int size) {
+    public ArrayBasedStackImplementation(int size) {
         this.size = size;
         array = new int[size];
         top = -1;
@@ -13,14 +13,18 @@ public class ArrayBasedStackImplementation implements Stack {
 
     @Override
     public int pop() {
-
-
-        return 0;
+        if (!isEmpty()) {
+            return array[top--];
+        }
+        return -1;
     }
 
     @Override
     public int peek() {
-        return 0;
+        if (!isEmpty()) {
+            return array[top];
+        }
+        return -1;
     }
 
     @Override
@@ -46,4 +50,206 @@ public class ArrayBasedStackImplementation implements Stack {
     public int size() {
         return size;
     }
+
+    public void printStack() {
+        if (!isEmpty()) {
+            ArrayBasedStackImplementation temp = new ArrayBasedStackImplementation(size);
+
+            while (!isEmpty()) {
+                System.out.print(array[top] + " ");
+                temp.push(pop());
+            }
+
+            System.out.println();
+            while (!temp.isEmpty()) {
+                push(temp.pop());
+            }
+        }
+    }
+
+    // recursively
+    public void printReversed() {
+        if (isEmpty()) {
+            System.out.println();
+            return;
+        }
+        int temp = pop();
+        printReversed();
+        System.out.print(temp + " ");
+        push(temp);
+    }
+
+//    public int max() {
+//        int max;
+//        while (!isEmpty()) {
+//            max = pop();
+//            if (max < peek()) {
+//                max = pop();
+//            }
+//            if (isEmpty()){
+//                return max;
+//            }
+//        }
+//        return 0;
+//    }
+
+    public int max() {
+        if (!isEmpty()) {
+            int max = 0;
+            for (int i = 0; i <= top; i++) {
+                if (max < array[i]) {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+        return 0;
+    }
+
+    public int min() {
+        if (!isEmpty()) {
+            int min = array[0];
+            for (int i = 0; i <= top; i++) {
+                if (min > array[i]) {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+        return 0;
+    }
+
+    public int avg() {
+        ArrayBasedStackImplementation temp = new ArrayBasedStackImplementation(size);
+        int sum = 0;
+        int counter = top + 1;
+
+        while (!isEmpty()) {
+            sum += peek();
+            temp.push(pop());
+        }
+
+        while (!temp.isEmpty()) {
+            push(temp.pop());
+        }
+        return sum / counter;
+    }
+
+    public boolean contains(int value) {
+        boolean conatins = false;
+        ArrayBasedStackImplementation temp = new ArrayBasedStackImplementation(size);
+
+        while (!isEmpty()) {
+            if (value == peek()) {
+                conatins = true;
+            }
+            temp.push(pop());
+        }
+
+        while (!temp.isEmpty()) {
+            push(temp.pop());
+        }
+        return conatins;
+    }
+
+    public boolean isEqual(ArrayBasedStackImplementation stack2) {
+        ArrayBasedStackImplementation temp = new ArrayBasedStackImplementation(size);
+        ArrayBasedStackImplementation tempStack2 = new ArrayBasedStackImplementation(stack2.size);
+
+        if (isEmpty() && stack2.isEmpty()) {
+            return true;
+        }
+        if (top == stack2.top) {
+            while (!isEmpty()) {
+                if (peek() != stack2.peek()) {
+                    return false;
+                }
+                temp.push(pop());
+                tempStack2.push(stack2.pop());
+            }
+            while (!temp.isEmpty()) {
+                push(temp.pop());
+                stack2.push(tempStack2.pop());
+            }
+        }
+        return true;
+    }
+
+    public boolean isReversee(ArrayBasedStackImplementation stack2) {
+        ArrayBasedStackImplementation temp = new ArrayBasedStackImplementation(size);
+        ArrayBasedStackImplementation tempStack2 = new ArrayBasedStackImplementation(stack2.size);
+
+        if (isEmpty() && stack2.isEmpty()) {
+            return true;
+        }
+        if (top == stack2.top) {
+            while (!stack2.isEmpty()) {
+                tempStack2.push(stack2.pop());
+            }
+            while (!isEmpty()) {
+                if (peek() != tempStack2.peek()) {
+                    return false;
+                }
+                temp.push(pop());
+                stack2.push(tempStack2.pop());
+            }
+            while (!temp.isEmpty()) {
+                push(temp.pop());
+            }
+        }
+        return true;
+    }
+
+    private void pushReversed(int temp) {
+        if (isEmpty()) {
+            push(temp);
+        } else {
+            int temp2 = pop();
+            pushReversed(temp);
+            push(temp2);
+        }
+    }
+
+    public void reverseStackRecursively() {
+        if (!isEmpty()) {
+            int temp = pop();
+            reverseStackRecursively();
+            pushReversed(temp);
+        }
+    }
+
+    public void sortStack() {
+        if (isEmpty()) {
+            return;
+        }
+
+        int temp = pop();
+        sortStack();
+        insertInSortedOrder(temp);
+    }
+
+    private void insertInSortedOrder(int temp) {
+        if (isEmpty() || peek() < temp) {
+            push(temp);
+        } else {
+            int temp2 = pop();
+            insertInSortedOrder(temp);
+            push(temp2);
+        }
+    }
+
+    public void sortStackUsingIteration() {
+        ArrayBasedStackImplementation tempStack = new ArrayBasedStackImplementation(size);
+        while (!isEmpty()) {
+            int temp = pop();
+            while (!tempStack.isEmpty() && tempStack.peek() > temp) {
+                push(tempStack.pop());
+            }
+            tempStack.push(temp);
+        }
+        while (!tempStack.isEmpty()) {
+            push(tempStack.pop());
+        }
+    }
+
 }
